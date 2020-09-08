644b379846f54702252a1530340bb5fb7f4018f5
Dario Carlino <baires82@gmail.com>
2020-09-08 02:30:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require "pry"
 
 class Song
   attr_accessor :name, :artist
-  
+
   def initialize(name)
     @name = name
     @@all << self
@@ -15,7 +15,7 @@ class Song
   def self.new_by_filename(file)
     cancion = file.split(" - ")
     nueva = Song.new(cancion[1])
-    nueva.artist_name = cancion[0]
+    
     nueva
 
   end
