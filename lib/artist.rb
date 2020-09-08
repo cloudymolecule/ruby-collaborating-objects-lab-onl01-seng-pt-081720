c5d1f0d30ba7c6e4431b7050895bcfe93ef964c5
Dario Carlino <baires82@gmail.com>
2020-09-08 03:15:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,17 +19,20 @@ class Artist
     @songs << song
   end
 
-  def self.create(name)
-    artist = self.new(name)
-    artist.name = name
-    artist
-  end
-
-  def self.find(name)
-    self.all.detect { |artist| artist.name == name }
-  end
+  # def self.create(name)
+  #   artist = self.new(name)
+  #   artist.name = name
+  #   artist
+  # end
+  #
+  # def self.find(name)
+  #   self.all.detect { |artist| artist.name == name }
+  # end
 
   def self.find_or_create_by_name(name)
-    self.find(name) || self.create(name)
+    self.all.detect do |art|
+      art.name == name
+      binding.pry
+    end
   end
 end
